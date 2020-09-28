8bf1e54129a907ceffc1441b0b048d81420171fc
alejandrodk <ottoabarriosp@hotmail.com>
2020-09-27 23:01:18 -0300

    Feature: Server clustering

--- src/bin/server.js
+++ src/bin/server.js
@@ -3,6 +3,8 @@ import App from '../app/app';
 import express from 'express';
 import debug from 'debug';
 import chalk from 'chalk';
+import cluster from 'cluster';
+import os from 'os';
 
 const app = express();
 const server = http.createServer(app);
@@ -59,8 +61,33 @@ api.init(app, server).then(async () => {
     console.log(chalk.green(`*-----------------------------*`));
     console.log(chalk.green(`| Server listen in port: ${port} |`));
     console.log(chalk.green(`| http://localhost:${port}       |`));
+    console.log(chalk.green(`| Master process pid: ${process.pid}   |`));
+    console.log(chalk.green(`| CPU cores Availables: ${os.cpus().length}    |`));
+    console.log(chalk.green(`| Workers Spawned: ${Object.keys(cluster.workers).length}          |`));
     console.log(chalk.green(`*-----------------------------*`));
   });
   server.on('error', onError);
   server.on('listening', onListening);
+
+  if (cluster.isMaster) {
+    const clustersQuantity = process.env.FORKS === 'MAX' ? os.cpus().length : process.env.FORKS || 0;
+    const workers = [];
+    // const worker = cluster.fork();
+    for (let i = 0; i < clustersQuantity; i++) {
+      workers.push(cluster.fork());
+      workers[i].on('message', function(message) {
+        console.log(message);
+      });
+    }
+    cluster.on('online', function(worker) {
+      console.log(chalk.green(`*-----------------------------*`));
+      console.log(chalk.green(`| worker created in pid ${worker.process.pid} |`));
+      console.log(chalk.green(`*-----------------------------*`));
+    });
+    cluster.on('exit', function(worker, code, signal) {
+      console.log(chalk.red(`Worker ${worker.process.pid} died with code ${code} and signal ${signal}`));
+      console.log(chalk.green('Starting new worker...'));
+      workers.push(cluster.fork());
+    });
+  }
 });
